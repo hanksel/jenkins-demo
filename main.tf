7feeac9c26edd86485c421d74dee52dfea9f8fd3
hanksel <hanksel_fm@hotmail.com>
2022-08-04 02:11:30 -0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -3,7 +3,10 @@ provider "google" {
  region      = "us-west1"
 }
 
-resource "google_sourcerepo_repository" "test_demo_jenkins" {
+
+
+resource "google_storage_bucket" "test_demo_jenkins" {
   name       = "test-demo-jenkins"
   project    = "mx-hanksel-test-generales"
+  location      = "US"
 }
